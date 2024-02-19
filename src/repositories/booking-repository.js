32fe1a7cb8563d96e1ae73e3532bc455ee9e7c87
yash7xm <yash.7xm@gmail.com>
2024-02-19 19:23:37 +0530

    booking payment service added

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 
 const { Booking } = require("../models");
 const CrudRepository = require("./crud-repository");
+const AppError = require("../utils/errors/app-error");
 
 class BookingRepository extends CrudRepository {
   constructor() {
@@ -10,6 +11,32 @@ class BookingRepository extends CrudRepository {
 
   async createBooking(data, transaction) {
     const response = await Booking.create(data, { transaction: transaction });
+    return response;
+  }
+
+  async get(data, transaction) {
+    const response = await this.model.findByPk(data, {
+      transaction: transaction,
+    });
+    if (!response) {
+      throw new AppError(
+        "Not able to found the resource",
+        StatusCodes.NOT_FOUND
+      );
+    }
+    return response;
+  }
+
+  async update(id, data, transaction) {
+    const response = await this.model.update(
+      data,
+      {
+        where: {
+          id: id,
+        },
+      },
+      { transaction: transaction }
+    );
     return response;
   }
 }
