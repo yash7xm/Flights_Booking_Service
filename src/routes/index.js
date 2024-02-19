d6eb9f537b62247a64d44b89109dc5ced1e68ff2
yash7xm <yash.7xm@gmail.com>
2024-02-19 16:08:54 +0530

    started booking implementation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 
-const v1Routes = require('./v1');
+const v1Routes = require("./v1");
 
 const router = express.Router();
 
-router.use('/v1', v1Routes);
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.use("/v1", v1Routes);
 
 module.exports = router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const { InfoController } = require('../../controllers');
+const { InfoController } = require("../../controllers");
 
 const router = express.Router();
 
-router.get('/info', InfoController.info);
+const bookingRoutes = require("./booking");
+
+router.get("/info", InfoController.info);
+
+router.use("/booking", bookingRoutes);
 
 module.exports = router;
